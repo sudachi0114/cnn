8baa68bd3ee1375018661b42b650f7d79f30d798
sudachi <jnevariste@gmail.com>
2019-10-01 01:57:48 +0000

    tee で log をとるために verbose=2 に変更。

--- binary_classifer/pad_binary_classify_full_learn.py
+++ binary_classifer/pad_binary_classify_full_learn.py
@@ -96,7 +96,8 @@ def main():
                                   steps_per_epoch=steps_per_epoch,
                                   epochs=50,
                                   validation_data=validation_generator,
-                                  validation_steps=validation_steps)
+                                  validation_steps=validation_steps,
+                                  verbose=2)
 
     # make log dir
     log_dir = os.path.join(cnn_dir, 'log')
