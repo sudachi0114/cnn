876716e6a24a73cec879ae9084be8dddc2440a99
sudachi <jnevariste@gmail.com>
2019-10-05 22:31:50 +0000

    log やモデルファイル/履歴ファイルなどの名前を自動で保存できるように変更。

--- binary_classifer/binary_classify_full_learn.py
+++ binary_classifer/binary_classify_full_learn.py
@@ -22,9 +22,11 @@ def main():
     input_size = 150
     batch_size = 32
 
-    # directory define
-    cwd = os.getcwd()
-    #print("current : ", cwd)
+    # directory defin -----
+    current_location = os.path.abspath(__file__)
+    cwd, base_name = os.path.split(current_location)
+    file_name, _ = os.path.splitext(base_name)
+    print("current location : ", cwd, ", this file : ", file_name)
 
     cnn_dir = os.path.dirname(cwd)
     data_dir = os.path.join(cnn_dir, "dogs_vs_cats_full")
@@ -36,7 +38,7 @@ def main():
     # make log dir
     log_dir = os.path.join(cwd, 'log')
     os.makedirs(log_dir, exist_ok=True)
-    child_log_dir = os.path.join(log_dir, "binary_classify_full_learn_log")
+    child_log_dir = os.path.join(log_dir, "{}_log".format(file_name))
     os.makedirs(child_log_dir, exist_ok=True)
     
     
@@ -97,11 +99,11 @@ def main():
                                   verbose=1)
 
     # save model & weights
-    model.save(os.path.join(child_log_dir, 'binary_dogs_vs_cats_full_learn_model.h5'))
+    model.save(os.path.join(child_log_dir, '{}_model.h5'.format(file_name)))
 
     # save history
     import pickle
-    with open(os.path.join(child_log_dir, 'binary_dogs_vs_cats_full_learn_history.pkl'), 'wb') as p:
+    with open(os.path.join(child_log_dir, '{}.pkl'.format(file_name)), 'wb') as p:
         pickle.dump(history.history, p)
                                   
     print("export logs in ", child_log_dir)
