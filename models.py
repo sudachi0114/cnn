11555c5f2dc984a551ca6e8c80fb7a34ed691bfe
sudachi <jnevariste@gmail.com>
2019-10-15 14:42:53 +0000

    model をビルドするプログラムをグローバルに定義してみよう

---
+++ models.py
@@ -0,0 +1,14 @@
+
+import os
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+class BuildModel:
+
+    def __init__(self):
+        print("instanced")
+
+if __name__ == '__main__':
+    model = BuildModel()
+    
