6ea981db75290726f37e664de3a529e59333f251
suzuki daichi <jnevariste@gmail.com>
2019-10-17 00:52:58 +0900

    class として定義するように変更

--- data_argumentation/errorAnalysis.py
+++ data_argumentation/errorAnalysis.py
@@ -2,7 +2,8 @@
 # 誤認識した画像をプロットして考察する:
 
 
-import os, datetime
+import os, sys, datetime
+sys.path.append(os.pardir)
 now = datetime.datetime.now()
 import numpy as np
 import pandas as pd
@@ -14,141 +15,183 @@ tf.Session(config=session_config)
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
-from keras.optimizers import Adam
 
-def testDataGenerator(test_dir, input_size, batch_size=10):
-
-    test_datagen = ImageDataGenerator(rescale=1.0/255.0)
-    test_generator = test_datagen.flow_from_directory(test_dir,
-                                                      target_size=(input_size, input_size),
-                                                      batch_size=batch_size,
-                                                      shuffle=False,
-                                                      class_mode='binary')
-    return test_generator
+from data_handler import DataHandler
 
 
+class errorAnalysis:
 
-def reloadModel(child_log_dir):
+    #def _dir_walker(self):
+    def __init__(self):
 
-    child_log_list = os.listdir(child_log_dir)
+        self.dirs = {}
+        self.dirs['cwd'] = os.getcwd()
+        self.dirs['cnn_dir'] = os.path.dirname(self.dirs['cwd'])
 
-    for f in child_log_list:
-        if "model" in f:
-            model_file = os.path.join(child_log_dir, f)
-    print("Use saved model : ", model_file)
+        # get test data -----
+        self.dirs['data_dir'] = os.path.join(self.dirs['cnn_dir'], "dogs_vs_cats_smaller")
+        #test_dir = os.path.join(data_dir, "test")
+        #print("test dir is in ... ", test_dir)
 
-    model = load_model(model_file, compile=True)
+        self.target_file = os.path.join(self.dirs['data_dir'], "test.npz")
 
-    return model
+        # get model file -----
+        self.dirs['log_dir'] = os.path.join(self.dirs['cwd'], "log")
+        child_log_list = os.listdir(self.dirs['log_dir'])
 
+        print("\nfind logs below -----")
+        for i, child in enumerate(child_log_list):
+            print(i, " | ", child)
 
+        print("\nPlease chose one child_log by index ...")
+        selected_child_log_idx = input(">>> ")
 
-def predStocker(generator, model, batch_size=10):
+        self.selected_child_log_dir = child_log_list[int(selected_child_log_idx)]
+        self.dirs['child_log_dir'] = os.path.join(self.dirs['log_dir'], self.selected_child_log_dir)
 
-    pred_steps = generator.n//batch_size    
-
-    # predict -----
-    pred_result = model.predict_generator(generator,
-                                          steps=pred_steps,
-                                          verbose=1)
-
-
-    # stock -----
-    pred_target, labels = [], []
-    for i in range(pred_steps):
-        tmp_target, tmp_label = next(generator)
-        if i == 0:
-            pred_target = tmp_target
-            labels = tmp_label
-        else:
-            pred_target = np.vstack((pred_target, tmp_target))
-            labels = np.hstack((labels, tmp_label))
+        print("\nuse log at ", self.dirs['child_log_dir'], "\n")
+        #print("this directory contain : ", os.listdir(child_log_dir))  # log list [history.pkl, model&weights.h5, log]
 
 
-    return pred_result, pred_target, labels  # 最初のクラスの予測値(確率)のnp配列, 予測target(画像), 正解label
+        child_log_list = os.listdir(self.dirs['child_log_dir'])
+
+        for f in child_log_list:
+            if "model" in f:
+                self.model_file = os.path.join(self.dirs['child_log_dir'], f)
+        print("Use saved model : ", self.model_file)
+
+    # old: def testDataGenerator(test_dir, input_size, batch_size=10):
+    def dataLoader(self):
+
+        data_handler = DataHandler()
+        npz = data_handler.npzLoader(self.target_file)
+
+        data, label = npz[0], npz[1]
+        data /= 255.0
+
+        return data, label
+
+
+    def reloadModel(self):
+
+        model = load_model(self.model_file, compile=True)
+
+        return model
+
+
+    def predictor(self, model, data, label):
+
+        pred_result = model.predict(data, verbose=1)
+
+        return pred_result  # クラス 1 の予測値(確率)のnp配列
+
+
+    def evaluator(self, model, data, label):
+
+        # wrong rate の check 用
+        print("\ncheck secence ...")
+
+        score = model.evaluate(data, label, verbose=1)
+
+        print("test accuracy: ", score[1])
+        print("test wrong rate must be (1-accuracy): ", 1.0-score[1])
 
 
 
-def main():
+    def displayConfuse(self, data, label, pred_result):
 
-    # 先にモデルを読み込み、モデルの input_shape を test_generator に渡す
-    model = reloadModel(child_log_dir)
-    #model.summary()    
+        # class 0 -> cat / class -> dog 変換
+        labels_class = []
+        for i in range(len(label)):
+            if label[i] == 0:
+                labels_class.append('cat')
+            elif label[i] == 1:
+                labels_class.append('dog')
 
-    input_size = model.inputs[0].shape[1]
-    test_generator = testDataGenerator(test_dir, input_size=input_size)
+        # 予測結果を表に起こす
+        pred = pd.DataFrame(pred_result, columns=['dog'])
+        pred['cat'] = 1.0 - pred['dog']
+        pred['class'] = pred.idxmax(axis=1)
+        pred['label'] = labels_class
+        pred['collect'] = (pred['class'] == pred['label'])
+        print(pred)
 
 
-    pred_result, pred_target, labels = predStocker(test_generator, model)
-    #print("pred_result : ", pred_result)
-    #print(labels)
+        confuse = pred[pred['collect'] == False].index.tolist()
+        print("\nwrong recognized indeices are ", confuse)
+        print("wrong recognized amount is ", len(confuse))
+        print("wrong rate : ", 100*len(confuse)/len(label), "%")
 
-    labels_class = []
-    for i in range(len(labels)):
-        if labels[i] == 0:
-            labels_class.append('cat')
-        elif labels[i] == 1:
-            labels_class.append('dog')
+        # display wrong classified photo -----
+        plt.figure(figsize=(7, 6))
+        plt.subplots_adjust(hspace=0.5)
+        plt.subplots_adjust(left=0.1, right=0.95, bottom=0.1, top=0.95)
+        #plt.title("Confusion picures #={}".format(len(confuse)))
 
-    #pred = pd.DataFrame(pred_result, columns=['cat'])
-    #pred['dog'] = 1.0 - pred['cat']
-    pred = pd.DataFrame(pred_result, columns=['dog'])
-    pred['cat'] = 1.0 - pred['dog']
-    pred['class'] = pred.idxmax(axis=1)
-    pred['label'] = labels_class
-    pred['collect'] = (pred['class'] == pred['label'])
-    print(pred)
+        for i, idx in enumerate(confuse):
+            plt.subplot(7, 7, 1+i)
+            plt.imshow(data[idx])
+            plt.axis(False)
+            plt.title("[{0}] p:{1}".format(idx, pred['class'][idx]))
+        img_file_place = os.path.join(self.dirs['child_log_dir'], "{0}_confusePics_{1:%y%m%d}_{2:%H%M}.png".format(self.selected_child_log_dir, now, now))
+        plt.savefig(img_file_place)
 
-    confuse = pred[pred['collect'] == False].index.tolist()
-    print("\nwrong recognized indeices are ", confuse)
-    print("wrong recognized amount is ", len(confuse))
-    print("wrong rate : ", 100*len(confuse)/len(labels), "%")
 
-    # wrong rate の check 用
-    print("\ncheck secence ...")
-    score = model.evaluate(pred_target, labels, verbose=1)
-    print("test accuracy: ", score[1])
-    print("test wrong rate must be (1-accuracy): ", 1.0-score[1])
-    
+    def displayCollect(self, data, label, pred_result):
 
-    plt.figure(figsize=(10, 10))
-    plt.subplots_adjust(hspace=0.5)
-    #plt.title("Confusion picures #={}".format(len(confuse)))
+        # class 0 -> cat / class -> dog 変換
+        labels_class = []
+        for i in range(len(label)):
+            if label[i] == 0:
+                labels_class.append('cat')
+            elif label[i] == 1:
+                labels_class.append('dog')
 
-    for i, idx in enumerate(confuse):
-        plt.subplot(7, 7, 1+i)
-        plt.imshow(pred_target[idx])
-        plt.axis(False)
-        plt.title("[{0}] p:{1}".format(idx, pred['class'][idx]))
-    img_file_place = os.path.join(child_log_dir, "{0}_confusePics_{1:%y%m%d}_{2:%H%M}.png".format(selected_child_log_dir, now, now))
-    plt.savefig(img_file_place)
+        # 予測結果を表に起こす
+        pred = pd.DataFrame(pred_result, columns=['dog'])
+        pred['cat'] = 1.0 - pred['dog']
+        pred['class'] = pred.idxmax(axis=1)
+        pred['label'] = labels_class
+        pred['collect'] = (pred['class'] == pred['label'])
+
+        collect = pred[pred['collect'] == True].index.tolist()
+
+        # display collect classified photo -----
+        plt.figure(figsize=(7, 6))
+        plt.subplots_adjust(hspace=0.5)
+        plt.subplots_adjust(left=0.1, right=0.95, bottom=0.1, top=0.95)
+        #plt.title("Confusion picures #={}".format(len(confuse)))
+
+        for i, idx in enumerate(collect):
+            plt.subplot(7, 7, 1+i)
+            plt.imshow(data[idx])
+            plt.axis(False)
+            plt.title("[{0}] p:{1}".format(idx, pred['class'][idx]))
+        img_file_place = os.path.join(self.dirs['child_log_dir'], "{0}_collectPics_{1:%y%m%d}_{2:%H%M}.png".format(self.selected_child_log_dir, now, now))
+        plt.savefig(img_file_place)
+
+
+
+    def do_whole(self):
+
+        model = self.reloadModel()
+        model.summary()
+
+        test_data, test_label = self.dataLoader()
+
+        pred_result = self.predictor(model, test_data, test_label)
+        #print("pred_result : ", pred_result)
+        #print(labels)
+
+        self.displayConfuse(test_data, test_label, pred_result)
+        #self.displayCollect(test_data, test_label, pred_result)
+
+        # wrong rate check
+        self.evaluator(model, test_data, test_label)
+
 
 
 if __name__ == '__main__':
 
-    cwd = os.getcwd()
-    cnn_dir = os.path.dirname(cwd)
-
-    data_dir = os.path.join(cnn_dir, "dogs_vs_cats_smaller")
-    test_dir = os.path.join(data_dir, "test")
-    print("test dir is in ... ", test_dir)
-    #print(len(os.listdir(test_dir)))  # 2 (dog/cat)
-
-    log_dir = os.path.join(cwd, "log")
-    child_log_list = os.listdir(log_dir)
-    
-    print("\nfind logs below -----")
-    for i, child in enumerate(child_log_list):
-        print(i, " | ", child)
-    print("\nPlease chose one child_log by index ...")
-    selected_child_log_idx = input(">>> ")
-    
-    selected_child_log_dir = child_log_list[int(selected_child_log_idx)]
-    child_log_dir = os.path.join(log_dir, selected_child_log_dir)
-    
-    print("\nuse log at ", child_log_dir, "\n")
-    print("this directory contain : ", os.listdir(child_log_dir))  # log list [history.pkl, model&weights.h5, log]
-
-    
-    main()
-    
+    task = errorAnalysis()
+    task.do_whole()
