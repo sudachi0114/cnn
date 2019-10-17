a8b718e3c64c41e1020882a1af980c2fb0df89b2
suzuki daichi <jnevariste@gmail.com>
2019-10-17 11:30:31 +0900

    deta full split の際にも test dir を作るように変更

--- director/full_separate.py
+++ director/full_separate.py
@@ -10,7 +10,7 @@ print("cwd : ", cwd)
 cnn_dir = os.path.dirname(cwd) # .../cnn
 print("cnn_dir : ", cnn_dir) 
 
-origin_data_dir = os.path.join(cnn_dir, "train")
+origin_data_dir = os.path.join(cnn_dir, "train")  # 今 test1 dir にある画像使ってない..
 print("origin_train_data_dir : ", origin_data_dir)
 
 # 分類先のディレクトリへの PATH
@@ -21,15 +21,23 @@ os.makedirs(base_dir, exist_ok=True)
 print('-*-'*10)
 
 # train / valid / test data を配置する dir
-#   validation は validation sprit で指定しようかな
-data_split = ["train", "validation"]
+data_split = ["train", "validation", "test"]
 
-# meta 
-data_amount = 12500
-validation_split = 0.1
+# meta -----
+# amount = 25000
+#   train = 20000
+#   val = 2500
+#   test = 2500
 
-validation_num = int(data_amount*validation_split)
-train_num = 12500 - validation_num
+class_label = ["cat", "dog"]
+
+amount_per_class = len(os.listdir(origin_data_dir))//len(class_label)
+train_num_per_class = 20000//len(class_label)
+validation_num_per_class = 2500//len(class_label)
+test_num_per_class = 2500//len(class_label)
+
+#validation_begin = train_num
+validation_end = train_num_per_class + validation_num_per_class
 
 for name in data_split:
 
@@ -38,8 +46,6 @@ for name in data_split:
     target_dir = os.path.join(base_dir, name)
     print("target_dir : ", target_dir)
     os.makedirs(target_dir, exist_ok=True)
-
-    class_label = ["cat", "dog"]
 
     for class_name in class_label:
         # train or validation /cats or dogs
@@ -50,17 +56,26 @@ for name in data_split:
 
         pic_name_list = []
         if name == "train":
-            print("Amount of {}/{} pictures is : {}".format(name, class_name, train_num))
-            for i in range(train_num):
+            print("Amount of {}/{} pictures is : {}".format(name, class_name, train_num_per_class))
+            print("train range is {} to {}".format(0, train_num_per_class))
+            for i in range(train_num_per_class):
                 pic_name_list.append("{}.{}.jpg".format(class_name, i))
             print("-> confirm : len(train_pic_name_list) = ", len(pic_name_list))
 
         elif name == "validation":
-            print("Amount of {}/{} pictures is : {}".format(name, class_name, validation_num))
-            for i in range(train_num, data_amount):
+            print("Amount of {}/{} pictures is : {}".format(name, class_name, validation_num_per_class))
+            print("validation range is {} to {}".format(train_num_per_class, validation_end))
+            for i in range(train_num_per_class, validation_end):
                 pic_name_list.append("{}.{}.jpg".format(class_name, i))
             print("-> confirm : len(test_pic_name_list) = ", len(pic_name_list))
-        
+
+        elif name == "test":
+            print("Amount of {}/{} pictures is : {}".format(name, class_name, amount_per_class-(train_num_per_class+validation_num_per_class)))
+            print("test range is {} to {}".format(validation_end, amount_per_class))
+            for i in range(validation_end, amount_per_class):
+                pic_name_list.append("{}.{}.jpg".format(class_name, i))
+            print("-> confirm : len(test_pic_name_list) = ", len(pic_name_list))
+
         #print("Copy name : {}/{} | pic_name_list : {}".format(name, class_name, pic_name_list))
 
         for pic_name in pic_name_list:
