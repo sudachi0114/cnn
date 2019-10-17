952f333627ffa299a5ef7c5cb705d4c20f6c4005
suzuki daichi <jnevariste@gmail.com>
2019-10-17 15:08:03 +0900

    da_handler を用いて学習用の画像を生成するように指定。

--- data_argumentation/train.py
+++ data_argumentation/train.py
@@ -8,60 +8,65 @@ session_config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
 tf.Session(config=session_config)
 
 from my_model import build_model
-#from datagen import trainDataGenerator, validationDataGenerator
-#from native_learn import trainDataGenerator, validationDataGenerator
-#from width_shift import trainDataGenerator, validationDataGenerator
-#from height_shift import trainDataGenerator, validationDataGenerator
-#from st_arg import trainDataGenerator, validationDataGenerator
-from horizontal_flip import trainDataGenerator, validationDataGenerator
-da_name = 'horizontal_flip'
+from da_handler import DaHandler
+
+chosen_mode = 'rotation'
 
 
 def train(set_epochs=50):
 
-    validation_generator = validationDataGenerator(validation_dir)
-    train_generator = trainDataGenerator(train_dir)
- 
+    da_handler = DaHandler()
+
+    validation_data, validation_label = da_handler.validationData()
+    #train_data, train_label = da_handler.keras_augment(mode=chosen_mode)
+    train_generator = da_handler.keras_augment(mode=chosen_mode)
+
     data_checker, label_checker = next(train_generator)
-    print("data shape : ", data_checker.shape)
-    print("label shape : ", label_checker.shape)
 
-    #print(data_checker[1])
-    #print(label_checker[1])
+    #print("train data shape : ", train_data.shape)
+    #print("train label shape : ", train_label.shape)
+    print("data_checker shape : ", data_checker.shape)
+    print("label_checker shape : ", label_checker.shape)
 
+    print("validation data shape : ", validation_data.shape)
+    print("validation label shape : ", validation_label.shape)
+
+
+    #INPUT_SIZE = train_data.shape[1]
     INPUT_SIZE = data_checker.shape[1]
     print("INPUT_SIZE: ", INPUT_SIZE)
-    
+
+    #CHANNEL = train_data.shape[3]
     CHANNEL = data_checker.shape[3]
     print("set channel : ", CHANNEL)
 
+    #batch_size = 10
     batch_size = data_checker.shape[0]
     print("batch_size : ", batch_size)
+
+    #data_size = train_data.shape[0]
+    #print("batch_size : ", batch_size)
 
     model = build_model(INPUT_SIZE, CHANNEL)
 
     model.summary()
-    
 
-    steps_per_epoch = train_generator.n // batch_size
-    validation_steps = validation_generator.n // batch_size
+    steps_per_epoch = train_generator.n//batch_size
     print(steps_per_epoch, " [steps / epoch]")
-    print(validation_steps, " (validation steps)")
 
 
     history = model.fit_generator(train_generator,
                                   steps_per_epoch=steps_per_epoch,
                                   epochs=set_epochs,
-                                  validation_data=validation_generator,
-                                  validation_steps=validation_steps,
+                                  validation_data=(validation_data, validation_label),
                                   verbose=1)
 
     # save model & weights
-    model_file = os.path.join(child_log_dir, '{}_model.h5'.format(da_name))
+    model_file = os.path.join(child_log_dir, '{}_model.h5'.format(chosen_mode))
     model.save(model_file)
 
     # save history
-    history_file = os.path.join(child_log_dir, '{}_history.pkl'.format(da_name))
+    history_file = os.path.join(child_log_dir, '{}_history.pkl'.format(chosen_mode))
     with open(history_file, 'wb') as p:
         pickle.dump(history.history, p)
 
@@ -87,8 +92,7 @@ if __name__ == '__main__':
     # make log dir -----
     log_dir = os.path.join(cwd, 'log')
     os.makedirs(log_dir, exist_ok=True)
-    child_log_dir = os.path.join(log_dir, da_name)
+    child_log_dir = os.path.join(log_dir, chosen_mode)
     os.makedirs(child_log_dir, exist_ok=True)
 
-    
     train()
