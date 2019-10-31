c19ba521b2cfd3901a935014a7fb62d37275d1d0
suzuki daichi <jnevariste@gmail.com>
2019-10-31 15:53:02 +0900

    da_concat のデータを用いて学習するように変更

--- data_augmentation/train.py
+++ data_augmentation/train.py
@@ -8,18 +8,31 @@ import tensorflow as tf
 session_config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
 tf.Session(config=session_config)
 
-from model_handler import ModelHandler
-from da_handler import DaHandler
-
-chosen_mode = 'doubled'
+from utils.model_handler import ModelHandler
+from utils.da_handler import DaHandler
 
 
-def train(set_epochs=50):
+cwd = os.getcwd()
+print("current location : ", cwd)
 
-    dh = DaHandler()
+cnn_dir = os.path.dirname(cwd)
+train_dir = os.path.join(cwd, "da_concat_31")
+validation_dir = os.path.join(cnn_dir, "dogs_vs_cats_smaller", "validation")
 
-    train_generator = dh.dataGeneratorFromDir(target_dir=train_dir)
-    validation_generator = dh.dataGeneratorFromDir(target_dir=validation_dir)
+# make log dir -----
+log_dir = os.path.join(cwd, 'log')
+os.makedirs(log_dir, exist_ok=True)
+
+
+
+def train(aug_no, model_mode='mymodel' ,set_epochs=10):
+
+    dah = DaHandler()
+
+    train_dir = os.path.join(cwd, "da_concat_{}".format(aug_no))
+    train_generator = dah.dataGeneratorFromDir(target_dir=train_dir)
+
+    validation_generator = dah.dataGeneratorFromDir(target_dir=validation_dir)
 
     data_checker, label_checker = next(train_generator)
 
@@ -37,19 +50,20 @@ def train(set_epochs=50):
     print("batch_size : ", batch_size)
 
 
-    mh = ModelHandler(INPUT_SIZE, CHANNEL)
-    model = mh.buildMyModel()
-    #model = mh.buildTlearnModel(base='mnv2')
-
-    model.summary()
-
     steps_per_epoch = train_generator.n // batch_size
     validation_steps = validation_generator.n // batch_size
     print(steps_per_epoch, " [steps / epoch]")
     print(validation_steps, " (validation steps)")
 
-    if chosen_mode == 'integrated' or 'doubled':
-        set_epochs /= 2
+    mh = ModelHandler(INPUT_SIZE, CHANNEL)
+
+    if model_mode == 'mymodel':
+        model = mh.buildMyModel()
+    elif model_mode == 'tlearn':
+        model = mh.buildTlearnModel(base='mnv1')
+
+    model.summary()
+
 
     history = model.fit_generator(train_generator,
                                   steps_per_epoch=steps_per_epoch,
@@ -58,44 +72,23 @@ def train(set_epochs=50):
                                   validation_steps=validation_steps,
                                   verbose=1)
 
+
+    child_log_dir = os.path.join(log_dir, '{}_{}'.format(aug_no, model_mode))
+    os.makedirs(child_log_dir, exist_ok=True)
+
     # save model & weights
-    model_file = os.path.join(child_log_dir, '{}_model.h5'.format(chosen_mode))
+    model_file = os.path.join(child_log_dir, '{}_{}_model.h5'.format(aug_no, model_mode))
     model.save(model_file)
 
     # save history
-    history_file = os.path.join(child_log_dir, '{}_history.pkl'.format(chosen_mode))
+    history_file = os.path.join(child_log_dir, '{}_{}_history.pkl'.format(aug_no, model_mode))
     with open(history_file, 'wb') as p:
         pickle.dump(history.history, p)
 
     print("export logs in ", child_log_dir)
 
-    
+
 if __name__ == '__main__':
 
-    current_location = os.path.abspath(__file__)  # このファイルの絶対パスを取得
-    cwd, base_name = os.path.split(current_location)  # path と ファイル名に分割
-    file_name, _ = os.path.splitext(base_name)  # ファイル名と拡張子を分離
-    print("current location : ", cwd, ", this file : ", file_name)
-
-    cnn_dir = os.path.dirname(cwd)
-
-    # 少ないデータに対して水増しを行いたいので smaller を選択
-    #data_dir = os.path.join(cnn_dir, "dogs_vs_cats_smaller")
-    if chosen_mode == 'integrated':
-        data_dir = os.path.join(cnn_dir, "dogs_vs_cats_integrated")
-    elif chosen_mode == 'doubled':
-        data_dir = os.path.join(cnn_dir, "dogs_vs_cats_smaller_doubled")
-    else:
-        data_dir = os.path.join(cnn_dir, "dogs_vs_cats_smaller")
-    train_dir = os.path.join(data_dir, "train")  # global
-    validation_dir = os.path.join(data_dir, "validation")  # global
-    print("train data is in ... ", train_dir)
-    print("validation data is in ...", validation_dir)
-
-    # make log dir -----
-    log_dir = os.path.join(cwd, 'log')
-    os.makedirs(log_dir, exist_ok=True)
-    child_log_dir = os.path.join(log_dir, chosen_mode)
-    os.makedirs(child_log_dir, exist_ok=True)
-
-    train()
+    # picked_aug_list = ["rotation", "hflip", "gnoise", "invert", "native"]
+    train(aug_no=31, model_mode='tlearn')
