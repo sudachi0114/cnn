296fb6b13e7ac825ce1cc0034c06ca9ec42af24c
suzuki daichi <jnevariste@gmail.com>
2019-11-08 12:36:15 +0900

    管理番号を指定してデータを作成できるように変更

--- exp_da_goodness/data_prep.py
+++ exp_da_goodness/data_prep.py
@@ -2,27 +2,35 @@
 import sys, os, shutil
 sys.path.append(os.pardir)
 
-from utils.data_separator import DataSeparator
-from utils.da_handler import DaHandler
+from utils.img_utils import inputDataCreator
+from utils.aug_with_imgaug import AugWithImgaug
 
 # 様々な変換を施したデータを生成
-dah = DaHandler()
+auger = AugWithImgaug()
 
 cwd = os.getcwd()
+prj_root = os.path.dirname(cwd)
+
 
 picked_aug_list = ["rotation", "hflip", "gnoise", "invert", "native"]
 # native を最後に置く事で、管理番号が奇数なら native が含まれている保証をできるようにする。
 
-classes = ["cat", "dog"]
+class_list = ["cat", "dog"]
 
 
 def CreateDaPictures():
 
     print("Create Augmented Pictures!")
 
+    target_dir = os.path.join(prj_root, "dogs_vs_cats_smaller", "train")
+
     for picked_aug in picked_aug_list:
-        print("\nprocess ", picked_aug, " .....")
-        dah.save_imgauged_img(save_dir=cwd, save_mode='image', aug=picked_aug)
+        print("\nprocess { ", picked_aug, " } .....")
+        auger.save_imgauged_img(target_dir,
+                                224,
+                                normalize=False,
+                                save_dir=cwd,
+                                aug=picked_aug)
         print("Done.")
 
 
@@ -42,7 +50,42 @@ def SieveDirList():
     return auged_data_dirs
 
 
-def daPictureConcatenate():
+def daPictureConcatenate(kanri_no):
+
+    # 組み合わせの "あるなし" を 2進法で取得
+    bin_kanri_no = format(kanri_no, '05b')  # 5桁 まで 0 埋め
+    print("\n[ ", kanri_no, " : ", bin_kanri_no, " ]")
+
+    # 対応するディレクトリを作成
+    concat_data_dir = os.path.join(cwd, "da_concat_{}".format(kanri_no))
+    os.makedirs(concat_data_dir, exist_ok=True)
+
+    selected_auged = []
+    for j in range(len(bin_kanri_no)):
+        if bin_kanri_no[j] == "1":  # flg が立っている場合は
+            selected_auged.append(picked_aug_list[j])  # そのDAパターンを選択
+            # print("    select: ", picked_aug_list[j])
+
+
+    # select されたものの中から
+    for aug_name in selected_auged:
+        cp_source_folder = "dogs_vs_cats_auged_{}".format(aug_name)  # 各フォルダの名前を作って
+
+        for class_name in class_list:  # 各クラスにアクセス
+            concat_target = os.path.join(cp_source_folder, class_name)  # ここがターゲット dogs_vs_cats_native/cat など
+            pic_list = os.listdir(concat_target)  # cp_source_folder にある写真のリストを取得
+            print("以下のファイルを移動します:\n", pic_list)
+            print("    個数: ", len(pic_list))
+
+            cp_dist_dir = os.path.join(concat_data_dir, class_name)  # 管理番号/cat などを作成
+            os.makedirs(cp_dist_dir, exist_ok=True)
+
+            for pic_name in pic_list:
+                copy_src = os.path.join(concat_target, pic_name)  # ココにある写真を
+                copy_dst = os.path.join(cp_dist_dir, pic_name)  # こっちにコピー
+                shutil.copy(copy_src, copy_dst)
+
+def concatWhole():
 
     auged_data_dirs = SieveDirList()
 
@@ -51,38 +94,9 @@ def daPictureConcatenate():
         auged_data_dirs = SieveDirList()
 
     for i in range(2**len(auged_data_dirs)):
+        daPictureConcatenate(i)
 
-        # 組み合わせの "あるなし" を 2進法で取得
-        bin_kanri_no = format(i, '05b')  # 5桁 まで 0 埋め
-        print("\n[ ", i, " : ", bin_kanri_no, " ]")
-
-        # 対応するディレクトリを作成
-        concat_data_dir = os.path.join(cwd, "da_concat_{}".format(i))
-        os.makedirs(concat_data_dir, exist_ok=True)
-
-        selected_auged = []
-        for j in range(len(bin_kanri_no)):
-            if bin_kanri_no[j] == "1":  # flg が立っている場合は
-                selected_auged.append(picked_aug_list[j])  # そのDAパターンを選択
-                # print("    select: ", picked_aug_list[j])
-
-
-        # select されたものの中から
-        for aug_name in selected_auged:
-            cp_source_folder = "dogs_vs_cats_auged_{}".format(aug_name)  # 各フォルダの名前を作って
-            for class_name in classes:  # 各クラスにアクセス
-                concat_target = os.path.join(cp_source_folder, class_name)  # ここがターゲット dogs_vs_cats_native/cat など
-                pic_list = os.listdir(concat_target)  # cp_source_folder にある写真のリストを取得
-                print("以下のファイルを移動します:\n", pic_list)
-                print("    個数: ", len(pic_list))
-
-                cp_dist_dir = os.path.join(concat_data_dir, class_name)  # 管理番号/cat などを作成
-                os.makedirs(cp_dist_dir, exist_ok=True)
-
-                for pic_name in pic_list:
-                    copy_src = os.path.join(concat_target, pic_name)  # ココにある写真を
-                    copy_dst = os.path.join(cp_dist_dir, pic_name)  # こっちにコピー
-                    shutil.copy(copy_src, copy_dst)
+        
 
 
 def clean():
@@ -114,12 +128,18 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Augment を 4種類行い、そのデータを保存するプログラム")
 
-    parser.add_argument("--make", action="store_true", help="augmented data creation.")
+    parser.add_argument("--make", action="store_true", help="augmented data creation & concat whole.")
+    parser.add_argument("--create_da_pics", action="store_true", help="augmented data creation.")
+    parser.add_argument("--create", default=32, type=int, help="concat augmented data.")
     parser.add_argument("--clean", action="store_true", help="remove auged datas.")
 
     args = parser.parse_args()
-
+    
+    if args.create_da_pics:
+        CreateDaPictures()
+    if args.create < 32:
+        daPictureConcatenate(args.create)
     if args.make:
-        daPictureConcatenate()
+        concatWhole()
     if args.clean:
         clean()
