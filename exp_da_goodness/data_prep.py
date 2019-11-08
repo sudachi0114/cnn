2b50d5a0be2e73874858763b11b448cdf1eb50c8
suzuki daichi <jnevariste@gmail.com>
2019-11-08 14:36:17 +0900

    引数に augment の種類を指定してデータを作ったりくっつけたりできるように変更

--- exp_da_goodness/data_prep.py
+++ exp_da_goodness/data_prep.py
@@ -18,20 +18,30 @@ picked_aug_list = ["rotation", "hflip", "gnoise", "invert", "native"]
 class_list = ["cat", "dog"]
 
 
-def CreateDaPictures():
+def CreateDaPictures(aug):
 
     print("Create Augmented Pictures!")
 
     target_dir = os.path.join(prj_root, "dogs_vs_cats_smaller", "train")
 
-    for picked_aug in picked_aug_list:
-        print("\nprocess { ", picked_aug, " } .....")
+    if type(aug) == str:
         auger.save_imgauged_img(target_dir,
                                 224,
                                 normalize=False,
                                 save_dir=cwd,
-                                aug=picked_aug)
+                                aug=aug)
         print("Done.")
+
+
+    elif type(aug) == list:
+        for picked_aug in aug:
+            print("\nprocess { ", picked_aug, " } .....")
+            auger.save_imgauged_img(target_dir,
+                                    224,
+                                    normalize=False,
+                                    save_dir=cwd,
+                                    aug=picked_aug)
+            print("Done.")
 
 
 def SieveDirList():
@@ -50,7 +60,34 @@ def SieveDirList():
     return auged_data_dirs
 
 
-def daPictureConcatenate(kanri_no):
+def someConcat(target_list):
+
+    # 対応するディレクトリを作成
+    concat_data_dir = os.path.join(cwd, "da_concat_some_{}".format(len(target_list)))
+    os.makedirs(concat_data_dir, exist_ok=True)
+
+
+    # select されたものの中から
+    for aug_name in target_list:
+        cp_source_folder = "dogs_vs_cats_auged_{}".format(aug_name)  # 各フォルダの名前を作って
+
+        for class_name in class_list:  # 各クラスにアクセス
+            concat_target = os.path.join(cp_source_folder, class_name)  # ここがターゲット dogs_vs_cats_native/cat など
+            pic_list = os.listdir(concat_target)  # cp_source_folder にある写真のリストを取得
+            print("\n以下のファイルを移動します:\n", pic_list)
+            print("    個数: ", len(pic_list))
+
+            cp_dist_dir = os.path.join(concat_data_dir, class_name)  # 管理番号/cat などを作成
+            os.makedirs(cp_dist_dir, exist_ok=True)
+
+            for pic_name in pic_list:
+                copy_src = os.path.join(concat_target, pic_name)  # ココにある写真を
+                copy_dst = os.path.join(cp_dist_dir, pic_name)  # こっちにコピー
+                shutil.copy(copy_src, copy_dst)
+
+
+
+def daPicsConcat(kanri_no):
 
     # 組み合わせの "あるなし" を 2進法で取得
     bin_kanri_no = format(kanri_no, '05b')  # 5桁 まで 0 埋め
@@ -74,7 +111,7 @@ def daPictureConcatenate(kanri_no):
         for class_name in class_list:  # 各クラスにアクセス
             concat_target = os.path.join(cp_source_folder, class_name)  # ここがターゲット dogs_vs_cats_native/cat など
             pic_list = os.listdir(concat_target)  # cp_source_folder にある写真のリストを取得
-            print("以下のファイルを移動します:\n", pic_list)
+            print("\n以下のファイルを移動します:\n", pic_list)
             print("    個数: ", len(pic_list))
 
             cp_dist_dir = os.path.join(concat_data_dir, class_name)  # 管理番号/cat などを作成
@@ -85,16 +122,17 @@ def daPictureConcatenate(kanri_no):
                 copy_dst = os.path.join(cp_dist_dir, pic_name)  # こっちにコピー
                 shutil.copy(copy_src, copy_dst)
 
+
 def concatWhole():
 
     auged_data_dirs = SieveDirList()
 
     if auged_data_dirs == []:
-        CreateDaPictures()
+        CreateDaPictures(picked_aug_list)
         auged_data_dirs = SieveDirList()
 
     for i in range(2**len(auged_data_dirs)):
-        daPictureConcatenate(i)
+        daPicsConcat(i)
 
         
 
@@ -128,18 +166,40 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Augment を 4種類行い、そのデータを保存するプログラム")
 
-    parser.add_argument("--make", action="store_true", help="augmented data creation & concat whole.")
-    parser.add_argument("--create_da_pics", action="store_true", help="augmented data creation.")
-    parser.add_argument("--create", default=32, type=int, help="concat augmented data.")
-    parser.add_argument("--clean", action="store_true", help="remove auged datas.")
+    parser.add_argument("--make",
+                        action="store_true",
+                        help="augmented data creation & concat >>whole<<.")
+    parser.add_argument("--create_from_list",
+                        action="store_true",
+                        help="augmented data creation by picked_aug_list = [rotation, hflip, gnoise, invert, native].")
+    parser.add_argument("--create_pics",
+                        nargs='*',
+                        help="augmented data creation define by your command line args.")
+    parser.add_argument("--some_concat",
+                        nargs='*',
+                        help="concat augmented data define by your command line args.")
+    parser.add_argument("--create",
+                        default=32,
+                        type=int,
+                        help="concat augmented data.")
+    parser.add_argument("--clean",
+                        action="store_true",
+                        help="remove auged datas.")
 
     args = parser.parse_args()
     
-    if args.create_da_pics:
-        CreateDaPictures()
+    if args.create_pics:
+        CreateDaPictures(args.create_pics)
+    if args.create_from_list:
+        CreateDaPictures(picked_aug_list)
+
+    if args.some_concat:
+        someConcat(args.some_concat)
     if args.create < 32:
-        daPictureConcatenate(args.create)
+        daPicsConcat(args.create)
+
     if args.make:
         concatWhole()
+
     if args.clean:
         clean()
