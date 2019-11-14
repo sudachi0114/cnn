e56ee2ab7d18b468d2482b065b2d0daff463745a
sudachi <jnevariste@gmail.com>
2019-11-14 22:46:22 +0000

    classmode categorical で動作確認

--- exp100rei/mini_train.py
+++ exp100rei/mini_train.py
@@ -2,15 +2,24 @@
 import os, sys
 sys.path.append(os.pardir)
 
-"""
-import tensorflow as tf
-session_config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
-tf.Session(config=session_config)
-"""
+import argparse
+
+parser = argparse.ArgumentParser(description="ミニマル学習プログラム")
+
+parser.add_argument("--debug", type=int, choices=[1, 2, 3],
+                    help="デバッグモードで実行 (Level を 1 ~ 3 の整数値で選択) ")
+
+args = parser.parse_args()
+
+debug_lv = args.debug
+if debug_lv == None:
+    debug_lv = 0
+
+
+import numpy as np
 
 import tensorflow as tf
 from keras.backend import tensorflow_backend
-
 config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
 session = tf.Session(config=config)
 tensorflow_backend.set_session(session)
@@ -18,8 +27,8 @@ tensorflow_backend.set_session(session)
 from utils.model_handler import ModelHandler
 from utils.img_utils import inputDataCreator
 
-from keras.callbacks import EarlyStopping
-from keras.preprocessing.image import ImageDataGenerator
+#from keras.callbacks import EarlyStopping
+#from keras.preprocessing.image import ImageDataGenerator
 
 cwd = os.getcwd()
 
@@ -35,23 +44,33 @@ train_data, train_label = inputDataCreator(train_dir,
                                            224,
                                            normalize=True)
 
+train_one_hot_label = np.identity(2)[train_label.astype(np.int8)]
 
-print("train_data: ", train_data.shape)
-print("train_label: ", train_label.shape)
 
-print(train_data[0])
-print(train_label)
+if debug_lv > 0:
+    print("train_data: ", train_data.shape)
+    print("train_label: ", train_label.shape)
+
+    print("\ntrain_one_hot_label: ", train_one_hot_label)
+
+    if debug_lv > 1:
+        print(train_data[0])
+        print(train_label)
 
 print("\ncreate validation data")
 validation_data, validation_label = inputDataCreator(validation_dir,
                                                      224,
                                                      normalize=True)
 
-print("validation_data: ", validation_data.shape)
-print("validation_label: ", validation_label.shape)
+validation_one_hot_label = np.identity(2)[validation_label.astype(np.int8)]
 
-print(validation_data[0])
-print(validation_label)
+if debug_lv > 0:
+    print("validation_data: ", validation_data.shape)
+    print("validation_label: ", validation_label.shape)
+
+    if debug_lv > 1:
+        print(validation_data[0])
+        print(validation_label)
 
 
 print("\ncreate test data")
@@ -59,12 +78,15 @@ test_data, test_label = inputDataCreator(test_dir,
                                          224,
                                          normalize=True)
 
-print("test_data: ", test_data.shape)
-print("test_label: ", test_label.shape)
+test_one_hot_label = np.identity(2)[test_label.astype(np.int8)]
 
-print(test_data[0])
-print(test_label)
+if debug_lv > 0:
+    print("test_data: ", test_data.shape)
+    print("test_label: ", test_label.shape)
 
+    if debug_lv > 1:
+        print(test_data[0])
+        print(test_label)
 
 
 mh = ModelHandler(224, 3)
@@ -75,12 +97,30 @@ model.summary()
 
 
 history = model.fit(train_data,
-                    train_label,
+                    train_one_hot_label,
                     batch_size=10,
-                    epochs=40,
-                    validation_data=(validation_data, validation_label),
+                    epochs=30,
+                    validation_data=(validation_data, validation_one_hot_label),
                     verbose=1)
 
 val_accs = history.history['val_accuracy']
 
 print(val_accs[len(val_accs)-1])
+
+print("\npredict sequence...")
+
+pred = model.predict(test_data,
+                     #test_one_hot_label,
+                     batch_size=10,
+                     verbose=1)
+
+print("result: ", pred)
+
+print("\nevaluate sequence...")
+
+eval_res = model.evaluate(test_data,
+                          test_one_hot_label,
+                          batch_size=10,
+                          verbose=1)
+
+print("result score: ", eval_res[1])
