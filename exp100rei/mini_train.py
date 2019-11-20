175cf44dfeb0ecad1ce772e4704609767286053d
sudachi <jnevariste@gmail.com>
2019-11-20 13:08:53 +0000

    predict の結果を整理, 結果出力用のdict を定義

--- exp100rei/mini_train.py
+++ exp100rei/mini_train.py
@@ -17,6 +17,7 @@ if debug_lv == None:
 
 
 import numpy as np
+import pandas as pd
 
 import tensorflow as tf
 #from keras.backend import tensorflow_backend
@@ -105,9 +106,12 @@ history = model.fit(train_data,
                     validation_data=(validation_data, validation_label),
                     verbose=1)
 
+accs = history.history['accuracy']
+losses = history.history['loss']
 val_accs = history.history['val_accuracy']
+val_losses = history.history['val_loss']
 
-print(val_accs[len(val_accs)-1])
+print("last val_acc: ", val_accs[len(val_accs)-1])
 
 print("\npredict sequence...")
 
@@ -116,7 +120,31 @@ pred = model.predict(test_data,
                      batch_size=10,
                      verbose=1)
 
-print("result: ", pred)
+label_name_list = []
+for i in range(len(test_label)):
+    if test_label[i][0] == 1:
+        label_name_list.append('cat')
+    elif test_label[i][1] == 1:
+        label_name_list.append('dog')
+        
+
+#print("result: ", pred)
+df_pred = pd.DataFrame(pred, columns=['cat', 'dog'])
+df_pred['class'] = df_pred.idxmax(axis=1)
+df_pred['label'] = pd.DataFrame(label_name_list, columns=['label'])
+df_pred['collect'] = (df_pred['class'] == df_pred['label'])
+
+confuse = df_pred[df_pred['collect'] == False].index.tolist()
+collect = df_pred[df_pred['collect'] == True].index.tolist()
+
+print(df_pred)
+print("\nwrong recognized indeices are ", confuse)
+print("  wrong recognized amount is ", len(confuse))
+print("\ncollect recognized indeices are ", collect)
+print("  collect recognized amount is ", len(collect))
+print("\nwrong rate: ", 100*len(confuse)/len(test_label), " %")
+
+
 
 print("\nevaluate sequence...")
 
@@ -125,4 +153,17 @@ eval_res = model.evaluate(test_data,
                           batch_size=10,
                           verbose=1)
 
+print("result loss: ", eval_res[0])
 print("result score: ", eval_res[1])
+
+# ----------
+save_dict = {}
+save_dict['last_loss'] = losses[len(losses)-1]
+save_dict['last_acc'] = accs[len(accs)-1]
+save_dict['last_val_loss'] = val_losses[len(val_losses)-1]
+save_dict['last_val_acc'] = val_accs[len(val_accs)-1]
+save_dict['n_confuse'] = len(confuse)
+save_dict['eval_loss'] = eval_res[0]
+save_dict['eval_acc'] = eval_res[1]
+
+print(save_dict)
