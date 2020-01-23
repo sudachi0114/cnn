ebc0e86761960aca085e428922dd7af59c5cf982
sudachi <jnevariste@gmail.com>
2020-01-23 17:30:52 +0000

    directory iterator がひとまず完成

--- utils/dirIterator.py
+++ utils/dirIterator.py
@@ -15,7 +15,9 @@ def dirsieve(dir_list):
 
 # define ----------
 cwd = os.getcwd()
-data_src = os.path.join(cwd, "small_721")
+prj_root = os.path.dirname(cwd)
+data_dir = os.path.join(prj_root, "datasets")
+data_src = os.path.join(data_dir, "small_721")
 
 # purpose_list = os.listdir(data_src)
 # purpose_list = dirsieve(purpose_list)
@@ -35,31 +37,88 @@ def get_target_list(target_dir):
         sub_target_dir = os.path.join(target_dir, cname)
         print(sub_target_dir)
 
+        # sub_target_list = os.listdir(sub_target_dir)
+
+        ## sub_target_list = []
+        ## sub_target_list += os.listdir(sub_target_dir) 
+
+        # sub_target_list = dirsieve(sub_target_list)
+        # return sub_target_list
+
         sub_target_list = os.listdir(sub_target_dir)
-        # target_list = dirsieve(target_list)
+        sub_target_list = dirsieve(sub_target_list)
+        full_path_list = []
+        for i in range(len(sub_target_list)):
+            fpath = os.path.join(sub_target_dir, sub_target_list[i])
+            full_path_list.append(fpath)
 
-        amount = len(sub_target_list)
+    ## sub_target_list = dirsieve(sub_target_list)
+    ## return sub_target_list
+    return full_path_list
 
-        return sub_target_list
 
+def diterator(target_dir, batch_size):
 
-def diterator(target_list, batch_size):
+    target_list = get_target_list(target_dir)
+    # ここに shuffle 処理を条件付きで入れる
 
     for i in range(steps):
-        begin = i * batch_size # 0, 10, 20, 30 ...
+        begin = i * batch_size  # 0, 10, 20, 30 ...
         end = begin + batch_size
         # print(target_list[begin:end])
 
-        yield target_list[begin:end]
-    
+        dbatch = target_list[begin:end]
 
+        yield dbatch
+
+
+def recalliterator(target_dir,
+                   batch_size,
+                   epochs
+                   # total_call,
+                   # iterator=diterator):
+                   ):
+
+    target_list = get_target_list(target_dir)
+    amount = len(target_list)
+    steps = amount // batch_size
+
+    total_call = steps*epochs
+
+    for i in range(total_call):
+        # if iteration counts over iterable num
+        #   re-instance iterator and generator start again
+        if (i % steps == 0):
+            dit = diterator(target_dir, batch_size)
+
+        batch = next(dit)
+        # print(batch)
+        # print(i, "(from recalliterator)")
+
+        yield batch
+
+
+# ここに dir (path) を渡したら
+#    画像を配列としての読み込み, 返す機能を加える
 if __name__ == "__main__":
 
-    target_list = get_target_list(train_dir)
+    
+    train_dir = os.path.join(data_src, "train")
 
+    target_list = get_target_list(train_dir)  # amount 計算用
+    amount = len(target_list)
+    print(target_list)
+    print(amount)
+
+    set_epochs = 3
     batch_size = 10
-    steps = 700 // batch_size
+    steps = amount // batch_size
 
-    for i in range(steps):
-        batch = next(diterator(target_list, batch_size))
-        print(batch)
+    # it = diterator(train_dir, batch_size)
+    reit = recalliterator(train_dir,
+                          batch_size=batch_size,
+                          epochs=set_epochs)  # 700//10 = 70, 70*3 = 210 total call
+    for i in range(steps*set_epochs):
+        over_batch = next(reit)
+        print(i, "|", over_batch)
+        print("  batch num: ", len(over_batch) )
