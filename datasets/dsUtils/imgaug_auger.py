a397043b8cc1e9c49cdb65db6a9b0ffc337bfd76
sudachi <jnevariste@gmail.com>
2020-06-08 02:11:36 +0000

    変数などの修正

--- datasets/dsUtils/imgaug_auger.py
+++ datasets/dsUtils/imgaug_auger.py
@@ -17,16 +17,13 @@ except:
 
 class AugWithImgaug:
 
-    def __init__(self, input_size=224, channel=3):
+    def __init__(self, INPUT_SIZE=224, CHANNEL=3, BATCH_SIZE=10):
 
         # 最低限の dir 構成を保持
         self.dirs = {}
-        """
         self.dirs['cwd'] = os.getcwd()
-        self.dirs['prj_root'] = os.path.dirname(self.dirs['cwd'])
-        self.dirs['data_dir'] = os.path.join(self.dirs['prj_root'], "datasets")
-        """
-        self.dirs['data_dir'] = os.getcwd()
+        self.dirs['datasets_dir'] = os.path.dirname(self.dirs['cwd'])
+    
 
         # list of imgaug DA modes -----
         self.imgaug_aug_list = ['native',
@@ -50,25 +47,25 @@ class AugWithImgaug:
         ]
 
         # attributes -----
-        self.BATCH_SIZE = 10
-        self.INPUT_SIZE = input_size
-        self.CHANNEL = channel
-        self.DO_SHUFFLE = True
-        self.CLASS_MODE = 'binary'
-        self.CLASS_LIST = ['cat', 'dog']
+        self.batch_size = BATCH_SIZE
+        self.input_size= INPUT_SIZE
+        self.channel = CHANNEL
+        self.DO_SHUFFLE = True # ?? FIXME (WHO AM I ?)
+        self.cls_list = ['cat', 'dog']
 
 
-    def img2array(self, target_dir, input_size, normalize=False):
+    def img2array(self, TARGET_DIR, INPUT_SIZE, NORMALIZE=False):
 
-        data, label = inputDataCreator(target_dir,
-                                       input_size,
-                                       normalize=normalize)
+        data, label = inputDataCreator(TARGET_DIR,
+                                       INPUT_SIZE,
+                                       normalize=NORMALIZE)
         return data, label
 
-    
-    def randomDataAugument(self, num_trans):
+
+
+    def randomDataAugument(self, NUM_TRANS):
         # 以下で定義する変換処理の内ランダムに幾つかの処理を選択
-        seq = iaa.SomeOf(num_trans, [
+        seq = iaa.SomeOf(NUM_TRANS, [
             iaa.Affine(rotate=(-90, 90), order=1, mode="edge"),
             iaa.Fliplr(1.0),
             iaa.OneOf([
@@ -97,9 +94,9 @@ class AugWithImgaug:
         return seq
 
 
-    def randomTestAugument(self, num_trans):
+    def randomTestAugument(self, NUM_TRANS):
         # 本番環境の危機から取得したデータにノイズが乗っていた状態を想定して変換
-        seq = iaa.SomeOf(num_trans, [
+        seq = iaa.SomeOf(NUM_TRANS, [
             iaa.Affine(rotate=(-90, 90), order=1, mode="edge"),
             iaa.Fliplr(1.0),
             iaa.OneOf([
@@ -128,43 +125,43 @@ class AugWithImgaug:
         return seq
 
 
-    def imgaug_augment(self, target_dir, input_size, normalize=False, aug='native'):
+    def imgaug_augment(self, TARGET_DIR, INPUT_SIZE, NORMALIZE=False, AUGMENTATION='native'):
 
-        data, label = self.img2array(target_dir, input_size, normalize)
+        data, label = self.img2array(TARGET_DIR, INPUT_SIZE, NORMALIZE)
 
-        if aug == 'native':
+        if AUGMENTATION == 'native':
             return data, label
-        elif aug == 'rotation':
+        elif AUGMENTATION == 'rotation':
             imgaug_aug = iaa.Affine(rotate=(-90, 90), order=1, mode="edge")  # 90度 "まで" 回転
-        elif aug == 'hflip':
+        elif AUGMENTATION == 'hflip':
             imgaug_aug = iaa.Fliplr(1.0)  # 左右反転
-        elif aug == 'width_shift':
+        elif AUGMENTATION == 'width_shift':
             imgaug_aug = iaa.Affine(translate_percent={"x": (-0.125, 0.125)}, order=1, mode="edge")  # 1/8 平行移動(左右)
-        elif aug == 'height_shift':
+        elif AUGMENTATION == 'height_shift':
             imgaug_aug = iaa.Affine(translate_percent={"y": (-0.125, 0.125)}, order=1, mode="edge")  # 1/8 平行移動(上下)
             # imgaug_aug = iaa.Crop(px=(0, 40))  <= 平行移動ではなく、切り抜き
-        elif aug == 'zoom':
+        elif AUGMENTATION == 'zoom':
             imgaug_aug = iaa.Affine(scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, order=1, mode="edge")  # 80~120% ズーム
             # これも keras と仕様が違って、縦横独立に拡大・縮小されるようである。
-        elif aug == 'logcon':
+        elif AUGMENTATION == 'logcon':
             imgaug_aug = iaa.LogContrast((0.5, 1.5))
-        elif aug == 'linecon':
+        elif AUGMENTATION == 'linecon':
             imgaug_aug = iaa.LinearContrast((0.5, 2.0))  # 明度変換
-        elif aug == 'gnoise':
+        elif AUGMENTATION == 'gnoise':
             imgaug_aug = iaa.AdditiveGaussianNoise(scale=[0.05*255, 0.2*255])  # Gaussian Noise
-        elif aug == 'lnoise':
+        elif AUGMENTATION == 'lnoise':
             imgaug_aug = iaa.AdditiveLaplaceNoise(scale=[0.05*255, 0.2*255])  # LaplaceNoise
-        elif aug == 'pnoise':
+        elif AUGMENTATION == 'pnoise':
             imgaug_aug = iaa.AdditivePoissonNoise(lam=(16.0, 48.0), per_channel=True)  # PoissonNoise
-        elif aug == 'flatten':
+        elif AUGMENTATION == 'flatten':
             imgaug_aug = iaa.GaussianBlur(sigma=(0.5, 1.0))  # blur: ぼかし (平滑化)
-        elif aug == 'sharpen':
+        elif AUGMENTATION == 'sharpen':
             imgaug_aug = iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)) # sharpen images (鮮鋭化)
-        elif aug == 'emboss':
+        elif AUGMENTATION == 'emboss':
             imgaug_aug = iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0))  # Edge 強調
-        elif aug == 'invert':
+        elif AUGMENTATION == 'invert':
             imgaug_aug = iaa.Invert(1.0)  # 色反転 <= これがうまく行かないので自分で作った。
-        elif aug == 'someof':  # 上記のうちのどれか1つ
+        elif AUGMENTATION == 'someof':  # 上記のうちのどれか1つ
             imgaug_aug = iaa.SomeOf(1, [
                 iaa.Affine(rotate=(-90, 90), order=1, mode="edge"),
                 iaa.Fliplr(1.0),
@@ -181,9 +178,9 @@ class AugWithImgaug:
                 iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)),
                 iaa.Invert(1.0)  # 14
             ])
-        elif aug == 'plural':  # 異なる系統の変換を複数(1つの変換あとに画素値がマイナスになるとError..)
+        elif AUGMENTATION == 'plural':  # 異なる系統の変換を複数(1つの変換あとに画素値がマイナスになるとError..)
             imgaug_aug = self.randomDataAugument(2)
-        elif aug == 'fortest':  # plural - invert (色反転) (test 用)
+        elif AUGMENTATION == 'fortest':  # plural - invert (色反転) (test 用)
             imgaug_aug = self.randomTestAugument(2)
         else:
             print("現在 imgaug で選択できる DA のモードは以下の通りです。")
@@ -197,47 +194,47 @@ class AugWithImgaug:
 
 
 
-    def save_imgauged_img(self, target_dir, input_size, normalize=False,
-                          save_dir=None, aug='rotation'):
+    def save_imgauged_img(self, TARGET_DIR, INPUT_SIZE, NORMALIZE=False,
+                          SAVE_DIR=None, AUGMENTATION='rotation'):
 
-        auged_data, label = self.imgaug_augment(target_dir=target_dir,
-                                                input_size=input_size,
-                                                normalize=normalize,
-                                                aug=aug)
-        if save_dir is None:
-            # origin_dir = os.path.basename(target_dir)
+        auged_data, label = self.imgaug_augment(TARGET_DIR=TARGET_DIR,
+                                                INPUT_SIZE=INPUT_SIZE,
+                                                NORMALIZE=NORMALIZE,
+                                                AUGMENTATION=AUGMENTATION)
+        if SAVE_DIR is None:
+            # origin_dir = os.path.basename(TARGET_DIR)
             # /home/user/cnn/datasets/some_721/train => [cat, dog]
-            #   pparent   : /home/user/cnn/datasets/some_721/
-            #   origin_dir: auged_train
-            pparent, origin_dir = os.path.split(target_dir)
-            self.dirs["save_dir"] = os.path.join(pparent,
-                                                 "{}_{}".format(aug, origin_dir))
+            #   par_dir   : /home/user/cnn/datasets/some_721/
+            #   dataset_name: auged_train
+            par_dir, dataset_name = os.path.split(TARGET_DIR)
+            save_dir = os.path.join(par_dir,
+                                    "{}_{}".format(AUGMENTATION, dataset_name))
         else:
-            self.dirs["save_dir"] = save_dir
-        os.makedirs(self.dirs["save_dir"], exist_ok=True)
+            save_dir = SAVE_DIR
+        os.makedirs(save_dir, exist_ok=False)
 
-        for j, class_name in enumerate(self.CLASS_LIST):
+        for j, cname in enumerate(self.cls_list):
             idx = 0
             for i, data in enumerate(auged_data):
                 if label[i] == j:
-                    save_dir_each =  os.path.join(self.dirs["save_dir"], '{}'.format(class_name))
-                    os.makedirs(save_dir_each, exist_ok=True)
-                    save_file = os.path.join(save_dir_each, "{}.{}.{}.jpg".format(class_name, aug, idx))
+                    cls_save_dir =  os.path.join(save_dir, cname)
+                    os.makedirs(cls_save_dir, exist_ok=True)
+                    save_file = os.path.join(cls_save_dir, "{}.{}.{}.jpg".format(cname, AUGMENTATION, idx))
 
                     pil_auged_img = Image.fromarray(data.astype('uint8'))  # float の場合は [0,1]/uintの場合は[0,255]で保存
                     pil_auged_img.save(save_file)
                     idx += 1
 
 
-    def display_imgaug(self, target_dir, input_size, normalize=False, aug="rotation"):
+    def display_imgaug(self, TARGET_DIR, INPUT_SIZE, NORMALIZE=False, AUGMENTATION="rotation"):
 
         for n_confirm in range(3):  # 三回出力して確認
             print("{}回目の出力".format(n_confirm+1))
             self.DO_SHUFFLE = False
-            data, label = self.imgaug_augment(target_dir,
-                                              input_size,
-                                              normalize,
-                                              aug=aug)
+            data, label = self.imgaug_augment(TARGET_DIR,
+                                              INPUT_SIZE,
+                                              NORMALIZE,
+                                              AUGMENTATION=AUGMENTATION)
             data = data / 255
 
             plt.figure(figsize=(12, 6))
@@ -266,20 +263,20 @@ if __name__ == '__main__':
     auger = AugWithImgaug()
 
     """
-    train_data, train_label = auger.img2array(train_dir, 224, normalize=False)
+    train_data, train_label = auger.img2array(train_dir, 224, NORMALIZE=False)
     print(train_data.shape)
     print(train_label.shape)
 
-    auged_data, label = auger.imgaug_augment(train_dir, 224, normalize=False, aug="invert")
+    auged_data, label = auger.imgaug_augment(train_dir, 224, NORMALIZE=False, AUGMENTATION="invert")
     print(auged_data.shape)
     print(label.shape)
 
-    auger.display_imgaug(train_dir, 224, normalize=False, aug="plural")
+    auger.display_imgaug(train_dir, 224, NORMALIZE=False, AUGMENTATION="plural")
     """
 
 
     for mode in ["train", "test"]:
-        for i in range(2):
+        for i in range(2):  # aug したのを 2回 geneる
             if mode == "train":
                 dname = "auged_train_{}".format(i)
                 target_dir = train_dir
@@ -293,8 +290,8 @@ if __name__ == '__main__':
             print(save_loc)
 
             auger.save_imgauged_img(target_dir,
-                                    input_size=224,
-                                    save_dir=save_loc,
-                                    aug=saug)
+                                    INPUT_SIZE=224,
+                                    SAVE_DIR=save_loc,
+                                    AUGMENTATION=saug)
 
 
